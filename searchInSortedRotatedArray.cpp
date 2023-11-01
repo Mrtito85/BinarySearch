02ee8e6fe45257a75055e2360eaed06cd5986cb6
Mrtito85 <114670888+Mrtito85@users.noreply.github.com>
2023-11-02 04:32:00 +0500

    Serarch in Rotated array using binary Search in Log (N

---
+++ searchInSortedRotatedArray.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int searchInRotatedArray(vector<int> &arr,int low,int high,int k){
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]==k)return mid;
+		//here we are checking over array is sorted or not
+
+		if(arr[low]<=arr[mid]){
+			//Here checking if over array is present in left half
+			if(arr[low]<=k && k<=arr[mid]){
+				high=mid-1;
+			}
+			else{
+				low=mid+1;
+			}
+		}else{
+			//Here checking over array in right half
+
+			if(arr[mid]<=k && k<=arr[high]){
+				low=mid+1;
+			}
+			else{
+				high=mid-1;
+			}
+
+		}
+	}
+	return -1;
+}
+ 
+int main() {
+
+
+	#ifndef ONLINE_JUDGE
+	freopen("input1.txt","r",stdin);
+	freopen("output1.txt","w",stdout);
+	#endif
+
+	int n,t;
+	cin>>n>>t;
+	vector<int> arr(n);
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	cout<<searchInRotatedArray(arr,0,arr.size()-1,t);
+
+
+    
+    return 0;
+}
